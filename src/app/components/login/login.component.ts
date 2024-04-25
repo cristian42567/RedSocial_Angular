ad09c01231abc3ea589c233cb384bdf81478c760
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-04-25 18:34:44 +0200

    formularios login actualizados

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DatosLogin } from '../../interfaces/datos-login';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { RegistroData } from '../../interfaces/registro-data';
 import { Router } from '@angular/router';
 
@@ -14,52 +14,96 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   showingRegisterForm: boolean = false;
 
   @Output() usuarioLogueado: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ){}
+    
 
+  ngOnInit(): void {
+    this.formularioLogin = this.formBuilder.group({
+      usuario: ["", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)
+      ]],
+      contraseña: ["", [
+        Validators.required,
+        Validators.minLength(7),
+        Validators.maxLength(14)
+      ]]
+    })
 
-  usuario: DatosLogin = {
-    usuario: "",
-    contrasena: ""
+    this.formularioRegistro = this.formBuilder.group({
+      usuario: ["", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)
+      ]],
+      contraseña: ["", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(14)
+      ]],
+      confirmarContraseña: ["", [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(14)
+      ]]
+    })
   }
 
-  registerData:RegistroData = {
-    username: "",
-    password: "",
-    confirmPassword:""
-  }
+  formularioLogin: FormGroup = new FormGroup({
+    usuario: new FormControl(""),
+    contraseña: new FormControl("")
+  })
+
+  formularioRegistro: FormGroup = new FormGroup({
+    usuario: new FormControl(""),
+    contraseña: new FormControl(""),
+    confirmarContraseña: new FormControl("")
+  })
+
 
   login() {
-    if(this.usuario.usuario == "cristian" && this.usuario.contrasena == "cristian"){
+    const nombreDeUsuario = this.formularioLogin.get("usuario")?.value
+    const contraseña = this.formularioLogin.get("contraseña")?.value
+
+    if(nombreDeUsuario == "admin" && contraseña == "admin"){
        this.usuarioLogueado.emit();
 
        this.router.navigate(["/posts"])
     }
 
-    this.usuario.usuario = ""
-    this.usuario.contrasena = ""
+    this.formularioLogin.get("usuario")?.setValue("")
+    this.formularioLogin.get("contraseña")?.setValue("")
 
   }
 
   registro() {
-    if(this.registerData.username == "" || this.registerData.password == "" || this.registerData.confirmPassword == ""){
-      alert("Los campos no pueden estar vacios")
-    }else if(this.registerData.password != this.registerData.confirmPassword){
+    const nombreDeUsuario = this.formularioRegistro.get("usuario")?.value
+    const contraseña = this.formularioRegistro.get("contraseña")?.value
+    const confirmarContraseña = this.formularioRegistro.get("confirmarContraseña")?.value
+
+    if(contraseña != confirmarContraseña){
       alert("Las contraseñas no coinciden")
     }else{
-      this.usuario.usuario =  this.registerData.username
-      this.usuario.contrasena =  this.registerData.password
+      this.formularioLogin.get("usuario")?.setValue(nombreDeUsuario)
+      this.formularioLogin.get("contraseña")?.setValue(contraseña)
 
       this.login()
+
+      console.log("Se ha registrado el usuario correctamente")
     }
 
-    this.registerData.username = ""
-    this.registerData.password = ""
-    this.registerData.confirmPassword = ""
+    this.formularioRegistro.get("usuario")?.setValue("")
+    this.formularioRegistro.get("contraseña")?.setValue("")
+    this.formularioRegistro.get("confirmarContraseña")?.setValue("")
   }
 
   toggleForm() {
